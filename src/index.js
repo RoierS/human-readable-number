58ebb1f4d1dae0f34dc40219f3976f78ecf95e98
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-01-08 14:32:10 +0100

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ module.exports = function toReadable (number) {
     
     if(number < 20) {
         return zeroToNineteen[number];
-    } else if(number == 20 | number == 30 |number == 40 |number == 50 | number == 60 | number == 70 | number == 80 | number == 90){
+    } else if((number == 20) || (number == 30) || (number == 40) || (number == 50) || (number == 60) || (number == 70) || (number == 80) || (number == 90)){
         return `${tenToNinety[tens - 1]}`;
-    } else if(number == 100 | number == 200 | number == 300 | number == 400 | number == 500 | number == 600 | number == 700 | number == 800 | number == 900) {
+    } else if(number == 100 || number == 200 || number == 300 || number == 400 || number == 500 || number == 600 || number == 700 || number == 800 || number == 900) {
         return `${zeroToNineteen[number / 100]} hundred`;
     } else if (number < 100) {
         return `${tenToNinety[tenToNinetyIndex]} ${zeroToNineteen[zeroToNineteenIndex]}`;
@@ -22,7 +22,7 @@ module.exports = function toReadable (number) {
         return `${zeroToNineteen[getZeroIndex]} hundred ${zeroToNineteen[getTeen]}`;
     } else if(getSecondIndex == '0'){
         return `${zeroToNineteen[getZeroIndex]} hundred ${tenToNinety[getFirstIndex]}`
-    } else if((number < 210 && number > 200) | (number < 310 && number > 300) | (number < 410 && number > 400) | (number < 510 && number > 500) | (number < 610 && number > 600) | (number < 710 && number > 700) | (number < 810 && number > 800) | (number < 910 && number > 900)) {
+    } else if((number < 210 && number > 200) || (number < 310 && number > 300) || (number < 410 && number > 400) || (number < 510 && number > 500) || (number < 610 && number > 600) || (number < 710 && number > 700) || (number < 810 && number > 800) || (number < 910 && number > 900)) {
         return `${zeroToNineteen[getZeroIndex]} hundred ${zeroToNineteen[Number(getSecondIndex)]}`;
     } else if(number < 1000) {
         return `${zeroToNineteen[getZeroIndex]} hundred ${tenToNinety[getFirstIndex]} ${zeroToNineteen[getSecondIndex]}`;
